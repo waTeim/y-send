f03ef333804b9f5f0b5bafb6220ab2860a2c84ee
waTeim <truthset@gmail.com>
2017-08-31 02:57:44 -0400

    Add encryption option

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let program = require('commander');
 program
   .usage('[options] <groupName> <channelName> <path>')
   .option('-d, --dev','send via dev')
+  .option('-e, --encrypted','send encrypted')
   .parse(process.argv);
 
 const doSend = Promise.coroutine(function *()
@@ -14,12 +15,15 @@ const doSend = Promise.coroutine(function *()
   let psyHost = "localhost";
   let apiHost = "api.esecuresend.com";
   let website = "www.esecuresend.com";
+  let encrypted = false;
 
   if(program.dev)
   { 
      apiHost = "api-dev.esecuresend.com";
      website = "dev.esecuresend.com";
   }
+
+  if(program.encrypted) encrypted = true;
 
   const psyloc = require('psyloc')(psyHost,apiHost,website);
 
@@ -28,7 +32,7 @@ const doSend = Promise.coroutine(function *()
 
     if(path.substr(0,1) != '/') path = __dirname + "/" + path;
 
-    let res = yield psyloc.sendViaChannel(program.args[0],program.args[1],path);
+    let res = yield psyloc.sendViaChannel(program.args[0],program.args[1],encrypted,path);
 
     console.log("res",JSON.stringify(res,null,2));
   }
